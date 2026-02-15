cd57de075b402d72b0d24d7fa642d3693fb6b7c2
wan.catherine <rarry1988528@126.com>
2026-02-14 19:41:14 -0500

    L0001 TwoSum

---
+++ src/main/java/org/wan/p0001_0100/L0001TwoSum.java
@@ -0,0 +1,17 @@
+package org.wan.p0001_0100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class L0001TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
---
+++ src/test/java/org/wan/p0001_0100/l0001TwoSumTest.java
@@ -0,0 +1,30 @@
+package org.wan.p0001_0100;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class L0001TwoSumTest {
+
+    L0001TwoSum twoSun = new L0001TwoSum();
+
+    @ParameterizedTest (name = "nums={0}, target={1}, expected={2}")
+    @MethodSource("provideTwoSumTestCases")
+    public void twoSumTest(int[] nums, int target, int[] expected) {
+        int[] result = twoSun.twoSum(nums, target);
+
+        assertArrayEquals(expected, result);
+    }
+
+    static Stream<Arguments> provideTwoSumTestCases() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1}),
+                Arguments.of(new int[]{3,2,4}, 6, new int[]{1, 2}),
+                Arguments.of(new int[]{3, 3}, 6, new int[]{0, 1})
+        );
+    }
+}
